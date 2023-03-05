3150e584044b5a5e00d5528335be0054630c891a
A. Maslarov <nsite2014@gmail.com>
2023-03-05 16:37:06 +0200

    Added Form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import styles from "./LoginFormmodule.css"
+
+export default function LoginForm() {
+
+    return(
+        <div>
+            <form className={styles.form}>
+                <label for="name" className={styles.label}>Name</label>
+                <input type="text" id="name" className={styles.input}></input>
+                <button className={styles.submit}>Submit</button>
+            </form>
+        </div>
+    );
+}
